10cb2236698d3451aa7ac8463ca29ba191b1bf82
hassanad <hassanad94@gmail.com>
2022-12-06 15:27:53 +0100

    init to node js backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const options = {
+  dotfiles: "ignore",
+  etag: true,
+  extensions: ["htm", "html"],
+  index: false,
+  maxAge: "7d",
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set("x-timestamp", Date.now());
+  },
+};
+
+app.use(express.static("public", options));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/main2.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
